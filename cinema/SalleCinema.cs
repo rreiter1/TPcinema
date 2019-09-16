01cd5507cd70d2fc514ce7ce04e2b62d0f68eea0
rreiter <rreiter@S-204-16>
2019-09-16 09:31:33 +0200

    Creation de la class SalleCinema avec ces caracteristiques et son constructeur

---
+++ cinema/SalleCinema.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cinema
+{
+    class SalleCinema
+    {
+        private string titre;
+        private int nbPlaces;
+        private double prixUnormal;
+        private int nbPlacesSelln;
+        private int nbPlacesSellr;
+
+
+        public SalleCinema(string t, int nbP, double prix)
+        {
+            this.titre = t;
+            this.nbPlaces = nbP;
+            this.prixUnormal = prix;
+            this.nbPlacesSelln = 0;
+            this.nbPlacesSellr = 0;
+        }
+
+    }
+}
