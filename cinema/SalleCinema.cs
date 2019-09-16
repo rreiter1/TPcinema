11ff8cda015bb3d34d736fa6ddeafa10801c3470
rreiter <rreiter@S-204-16>
2019-09-16 10:31:18 +0200

    Methode de la class SalleCinema (fini juste dans la methode vendrePlaces a definir la textbox ou iras les message d'erreure ou de validation)

--- cinema/SalleCinema.cs
+++ cinema/SalleCinema.cs
@@ -23,6 +23,43 @@ namespace cinema
             this.nbPlacesSelln = 0;
             this.nbPlacesSellr = 0;
         }
-
+        public int nbPlacesDisponibles()
+        {
+            return this.nbPlaces - (this.nbPlacesSelln + this.nbPlacesSellr);
+        }
+        public void vendrePlaces(int nbre, bool tarifReduit)
+        {
+            if ((this.nbPlacesDisponibles() - nbre) >= 0)
+            {
+                string textPrixaPayer;
+                if(tarifReduit)
+                {
+                    this.nbPlacesSellr += nbre;
+                    textPrixaPayer = "Le Prix a Payer est de "+((this.prixUnormal*0.8)*nbre);
+                }
+                else
+                {
+                    this.nbPlacesSelln += nbre;
+                    textPrixaPayer = "Le prix a Payer est de " + (this.prixUnormal * nbre);
+                }
+            }
+            else
+            {
+                string rejet = "Nous ne disposont pas assez de place pour tous ces gens nous avont "+ this.nbPlacesDisponibles() + " de disponible";
+            }
+        }        public void remiseAZero()
+        {
+            this.nbPlacesSelln = 0;
+            this.nbPlacesSellr = 0;
+        }        public double chiffreAffaires()
+        {
+            return ((this.prixUnormal * 0.8) * this.nbPlacesSellr) + (this.prixUnormal * this.nbPlacesSelln);
+        }        public double tauxRemplissage()
+        {
+            int nbplaceVendu = (nbPlacesSelln + nbPlacesSellr);
+            return nbplaceVendu / this.nbPlaces * 100;
+        }        public String toString()
+        {
+            return "Pour une salle de " + this.nbPlaces + " places jouant le film '" + this.titre + "' dont " + this.nbPlacesSelln + " places ont été vendues au tarif normal" + this.prixUnormal+ " et " + this.nbPlacesSellr + " places ont été vendues au tarif réduit";        }
     }
 }
